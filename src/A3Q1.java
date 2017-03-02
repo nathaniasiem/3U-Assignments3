9a870b3caa777657b0e716adf152e3cfc1eaf986
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-03-02 09:27:36 -0500

    Assignment 3-modified

--- src/A3Q1.java
+++ src/A3Q1.java
@@ -9,6 +9,7 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  *
  * @author simon7323
@@ -19,31 +20,25 @@ public class A3Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // create a new city
+        // create new city
         City kw = new City();
-
-        //create new Robot
-        RobotSE karel = new RobotSE(kw, 2, 0, Direction.EAST);
-
+        
+        //create new robot
+        RobotSE karel= new RobotSE(kw,2,0,Direction.EAST);
+        
+        //place things blocking robot
+        new Thing(kw,2,4);
+        new Thing(kw,2,5);
+        
         //create a wall
-        new Wall(kw, 2, 6, Direction.WEST);
-
-        //place things
-        new Thing(kw, 2, 4);
-        new Thing(kw, 2, 5);
-
-        //
-        while (true) {
-            if (!karel.canPickThing()) {
-                karel.move();
-            }
+        new Wall(kw,2,6,Direction.WEST);
+        
+        //move robot until reach thing
+        while(true){
+        if(!(karel.canPickThing())){
+            karel.move();
         }
+        
     }
-        //create another situation with new Robot
-        //  create new Robot
-        RobotSE marie = new RobotSE (kw,4,0,Direction.EAST);
-    private City kw;
-        //set robot colour
-                marie.setColor(Color.pink);
-                
+}
 }
