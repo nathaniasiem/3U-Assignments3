93400efaffc85ea4cc32fa3d552f64156e1f17f8
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-03-08 09:38:20 -0500

    A3Q4 completed with comments

--- src/A3Q4.java
+++ src/A3Q4.java
@@ -39,36 +39,60 @@ public class A3Q4 {
         new Wall(kw, 2, 3, Direction.EAST);
 
         //program karel to find exit
+        //create loop
         while (true) {
+            //if karel is not blocked
+            //karel moves
             if (karel.frontIsClear()) {
                 karel.move();
+            //if karel is blocked
+            //karel turns around
             } else {
                 karel.turnAround();
             }
+            //if the intersection karel is on is (1,2)
+            //stop
             if (karel.getStreet() == 1 && karel.getAvenue() == 2) {
                 break;
             }
         }
         //create another program for situation 2
         //create another robot
-        RobotSE nia = new RobotSE(kw,6,1,Direction.EAST);
+        RobotSE nia = new RobotSE(kw, 6, 1, Direction.EAST);
         nia.setColor(Color.blue);
-        
+
         //build a room for nia
-        new Wall(kw,6,1,Direction.NORTH);
-        new Wall(kw,6,2,Direction.NORTH);
-        new Wall(kw,6,3,Direction.NORTH);
-        new Wall(kw,6,1,Direction.WEST);
-        new Wall(kw,7,1,Direction.WEST);
-        new Wall(kw,8,1,Direction.WEST);
-        new Wall(kw,8,1,Direction.SOUTH);
-        new Wall(kw,8,2,Direction.SOUTH);
-        new Wall(kw,8,3,Direction.SOUTH);
-        
-        
-        
-        
-        
-        
+        new Wall(kw, 6, 1, Direction.NORTH);
+        new Wall(kw, 6, 2, Direction.NORTH);
+        new Wall(kw, 6, 3, Direction.NORTH);
+        new Wall(kw, 6, 1, Direction.WEST);
+        new Wall(kw, 7, 1, Direction.WEST);
+        new Wall(kw, 8, 1, Direction.WEST);
+        new Wall(kw, 8, 1, Direction.SOUTH);
+        new Wall(kw, 8, 2, Direction.SOUTH);
+        new Wall(kw, 8, 3, Direction.SOUTH);
+        new Wall(kw, 8, 3, Direction.EAST);
+        new Wall(kw, 6, 3, Direction.EAST);
+
+        //program nia to find exit
+        //create loop
+        while (true) {
+            //if nia is not blocked
+            //nia moves
+            if (nia.frontIsClear()) {
+                nia.move();
+            //if nia is blocked
+            //nia turns to avoid barrier
+            } else {
+                nia.turnRight();
+                nia.move();
+                nia.turnLeft();
+            }
+            //if the intersection nia is on is (7,4)
+            //nia stops
+            if (nia.getStreet() == 7 && nia.getAvenue() == 4 ) {
+                break;
+            }
+        }
     }
 }
